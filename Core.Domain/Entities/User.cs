6649b27bbb6bec3164e0b2cd26425cfca841c35d
furkanOral-git <furkan09biricik@gmail.com>
2022-11-26 22:41:44 +0300

    User class moved to domain layer

--- Core.Domain/Entities/User.cs
+++ Core.Domain/Entities/User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Domain.Entities;
 
-namespace Core.Security.Entities
+namespace Core.Domain.Entities
 {
     public class User : Entity
     {
